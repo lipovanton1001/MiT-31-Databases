c6f54c0df50726ad17f1da00682bf6767a0b238f
lipovanton1001 <136848514+lipovanton1001@users.noreply.github.com>
2026-02-10 20:13:36 +0200

    Delete Lab1_DataBase.sql

--- Lab1_DataBase.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    c.full_name AS "Клієнт",
-    s.title AS "Абонемент",
-    t.name AS "Тренер",
-    t.specialty AS "Напрямок"
-FROM clients c
-JOIN subscriptions s ON c.subscription_id = s.subscription_id
-JOIN trainers t ON c.trainer_id = t.trainer_id;
